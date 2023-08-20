ad65f43850b8c19b54e5e3b6d6a79a3df999a2cc
schriftgestalt <georg.seifert@mac.com>
2023-08-20 11:25:59 +0200

    whitespace

--- Text View Width.glyphsPalette/Contents/Resources/plugin.py
+++ Text View Width.glyphsPalette/Contents/Resources/plugin.py
@@ -13,19 +13,19 @@ class TextViewWidth (PalettePlugin):
 	def settings(self):
 		self.name = Glyphs.localize({
 			'en': 'Text View Width',
-			})
-		
+		})
+
 		M = 10
 		width = 150
-		height = M*2.5
+		height = M * 2.5
 		self.paletteView = Window((width, height))
 		self.paletteView.group = Group((0, 0, width, height))
-		self.paletteView.group.editText = EditText((M, 0, M*5, M*2),
+		self.paletteView.group.editText = EditText((M, 0, M * 5, M * 2),
 						callback=self.editTextCallback,
 						sizeStyle = 'small',
 						placeholder = 'None',
 						continuous = False)
-		self.paletteView.group.slider = Slider((M*7, 0, -M, M*2),
+		self.paletteView.group.slider = Slider((M * 7, 0, -M, M * 2),
 						callback=self.sliderCallback,
 						minValue=1000,
 						maxValue=30000,
@@ -33,83 +33,78 @@ class TextViewWidth (PalettePlugin):
 		self.dialog = self.paletteView.group.getNSView()
 
 		# self.loadPreferences()
-		self.updateWidth( Glyphs.font )
+		self.updateWidth(Glyphs.font)
 
 	@objc.python_method
 	def start(self):
 		# Adding a callback for the 'GSUpdateInterface' event
 		Glyphs.addCallback(self.update, UPDATEINTERFACE)
 
-	@objc.python_method	
+	@objc.python_method
 	def __del__(self):
 		Glyphs.removeCallback(self.update)
-	
 
 	@objc.python_method
-	def loadPreferences( self ):
+	def loadPreferences(self):
 		try:
 			# set edit text
-			self.paletteView.group.editText.set( str( Glyphs.defaults["com.slobzheninov.textEditWidth"]) )
-			
+			self.paletteView.group.editText.set(str(Glyphs.defaults["com.slobzheninov.textEditWidth"]))
+
 			return Glyphs.defaults["com.slobzheninov.textEditWidth"]
-		except:			
+		except:
 			return None
 
-
 	@objc.python_method
-	def savePreferences( self, userInput ):
+	def savePreferences(self, userInput):
 		try:
 			if userInput:
 				Glyphs.defaults["com.slobzheninov.textEditWidth"] = userInput
 			else:
-				del( Glyphs.defaults["com.slobzheninov.textEditWidth"] )
+				del(Glyphs.defaults["com.slobzheninov.textEditWidth"])
 		except:
 			print ("Could not save preferences")
-			print( traceback.format_exc() )
-
+			print(traceback.format_exc())
 
 	@objc.python_method
-	def editTextCallback( self, sender ):
+	def editTextCallback(self, sender):
 		userInput = sender.get()
-		self.savePreferences( userInput )
+		self.savePreferences(userInput)
 		font = Glyphs.font
-		self.updateWidth( font )
+		self.updateWidth(font)
 
 	@objc.python_method
-	def sliderCallback( self, sender ):
+	def sliderCallback(self, sender):
 		userInput = int(sender.get())
-		self.savePreferences( userInput )
+		self.savePreferences(userInput)
 		font = Glyphs.font
-		self.updateWidth( font )
+		self.updateWidth(font)
 
 	@objc.python_method
 	def update(self, sender):
 		font = Glyphs.font
-		self.updateWidth( font )
-
+		self.updateWidth(font)
 
 	@objc.python_method
-	def updateWidth( self, font ):
+	def updateWidth(self, font):
 		userInput = self.loadPreferences()
 		if font:
 			if userInput:
 				width = None
 
 				# int input or slider
-				try:					
-					width = int( userInput )
-	
-				
+				try:
+					width = int(userInput)
+
 				# input in characters (10 n)
 				except:
 					userInputSplitted = userInput.split()
 					if userInputSplitted[0].isnumeric() and userInputSplitted[1].isalpha():
 						if userInputSplitted[1] in font.glyphs:
-							glyph = font.glyphs[ userInputSplitted[1] ]
+							glyph = font.glyphs[userInputSplitted[1]]
 							times = float(userInputSplitted[0])
 							master = font.selectedFontMaster
-							if glyph.layers[ master.id ].width > 0:
-								width = times * glyph.layers[ master.id ].width
+							if glyph.layers[master.id].width > 0:
+								width = times * glyph.layers[master.id].width
 							else:
 								print('Text View Width plugin input error:\nGlyph %s (%s) has 0 width' % (glyph.name, master.name))
 								Glyphs.showMacroWindow()
@@ -124,12 +119,11 @@ class TextViewWidth (PalettePlugin):
 					Glyphs.intDefaults["GSFontViewWidth"] = int(width)
 
 					# set slider position
-					self.paletteView.group.slider.set( int( width ))
+					self.paletteView.group.slider.set(int(width))
 
 			# show current value as the placeholder
 			elif Glyphs.intDefaults["GSFontViewWidth"]:
-				self.paletteView.group.editText.setPlaceholder( str(Glyphs.intDefaults["GSFontViewWidth"]) )
-
+				self.paletteView.group.editText.setPlaceholder(str(Glyphs.intDefaults["GSFontViewWidth"]))
 
 	@objc.python_method
 	def __file__(self):
